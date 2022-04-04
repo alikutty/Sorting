44bfc49ca8e02803ff8e5527ba2d17841819bc6b
kutty <kutty@10.12.13.212>
2022-04-04 12:14:10 +0530

    new sort

---
+++ src/package1/Newsort2.java
@@ -0,0 +1,33 @@
+package package1;
+
+public class Newsort2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Custom input array
+				int arr[] = { 4, 3, 2, 1 };
+
+				// Outer loop
+				for (int i = 0; i < arr.length; i++) {
+
+					// Inner nested loop pointing 1 index ahead
+					for (int j = i + 1; j < arr.length; j++) {
+
+						// Checking elements
+						int temp = 0;
+						if (arr[j] < arr[i]) {
+
+							// Swapping
+							temp = arr[i];
+							arr[i] = arr[j];
+							arr[j] = temp;
+						}
+					}
+
+					// Printing sorted array elements
+					System.out.print(arr[i] + " ");
+				}
+			}
+		}
+
